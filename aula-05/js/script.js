d554dfd2cf5505d3a6cc9777a5a814c96a9b6ec8
devdantas <mauriciodantas.contato01@bol.com.br>
2019-11-05 21:59:51 -0300

    adicionando aula-o5

---
+++ aula-05/js/script.js
@@ -0,0 +1,33 @@
+let user = "admin"
+let password = "admin"
+
+// let userVal = prompt('Insira o usuario')
+// let passVal = prompt('Insira a senha')
+
+let trys = 3
+let ver = false
+
+while (trys>=0) {
+    let userVal = prompt('Insira o usuario')
+    let passVal = prompt('Insira a senha')
+
+    if(userVal === userVal && passVal === password) {
+        alert("Logado com sucesso")
+    } else {
+        alert('Usuario ou senha incorretos')
+        trys = trys - 1
+    }
+}
+console.log(trys)
+if(trys === -1) {
+    alert('Suas tentativas acabaram')
+}
+
+
+// Exercicio 01
+
+/* if(userVal === userVal && passVal === password) {
+    alert("Logado com sucesso")
+} else {
+    alert("Usuario ou senha incorretos")
+} */

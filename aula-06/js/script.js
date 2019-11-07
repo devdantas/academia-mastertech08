8ceddab6b8d859fe9c53c6a0b2977013d63b26fa
devdantas <mauriciodantas.contato01@bol.com.br>
2019-11-06 21:53:33 -0300

    Exercise Stairs

---
+++ aula-06/js/script.js
@@ -0,0 +1,21 @@
+let animes = ['Death Note', 'Boku no Hero', 'Dragon Ball', 'Cavaleiros do Zodiaco', 'Naruto', 'Super onze', 'Nazo no Kanojo']
+
+// let i = 0
+
+/* while(i < animes.length) {
+    console.log(animes[i])
+    i++
+}
+
+/* for(let e = 0; e < animes.length; e++) {
+    console.log(animes[e])
+} */
+
+let caracter = prompt('Insira o caracter para escada')
+let degraus = Number(prompt('Insira a quantidade de degraus'))
+let auxiliar = ""
+for(let i=0; i < degraus; i++) {
+    console.log(auxiliar = auxiliar + caracter)
+}
+
+

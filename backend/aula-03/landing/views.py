6bee77ee0ad13bfce409e7e4e9f77511c3086b86
devdantas <mauriciodantas.contato01@bol.com.br>
2019-11-14 22:02:18 -0300

    aprendendo rotas

--- backend/aula-03/landing/views.py
+++ backend/aula-03/landing/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def mostrar_index(req):
+    return render(req, 'index.html')
+
+def mostrar_sobre(req):
+    return render(req, 'sobre.html')
